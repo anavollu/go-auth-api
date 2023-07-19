9e711da95f5931dc162a1bc52c5778c80b72f9e6
anavollu <anavollu@id.uff.br>
2023-07-19 08:28:06 -0300

    wip: add cognito, /login, /changepassword, /home

--- main.go
+++ main.go
@@ -1,17 +1,189 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"github.com/gin-gonic/gin"
 )
 
+type Cookie struct {
+	Name string
+}
+
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (ck Cookie) Set(c *gin.Context, token string) {
+	cookie := http.Cookie{
+		Name:     ck.Name,
+		Value:    token,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+		Secure:   true,
+		Path:     "/",
+	}
+	http.SetCookie(c.Writer, &cookie)
+}
+
+func (ck Cookie) Get(c *gin.Context) (string, error) {
+	cookie, err := c.Request.Cookie(ck.Name)
+	if err != nil {
+		return "", err
+	}
+	return cookie.Value, nil
+}
+
+func (ck Cookie) Clear(c *gin.Context) {
+	cookie := http.Cookie{
+		Name:     ck.Name,
+		Value:    "",
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+		Secure:   true,
+		Path:     "/",
+		MaxAge:   -1,
+	}
+	http.SetCookie(c.Writer, &cookie)
+}
+
+const flowUsernamePassword = "USER_PASSWORD_AUTH"
+const flowRefreshToken = "REFRESH_TOKEN_AUTH"
+
+func computeSecretHash(clientSecret string, username string, clientId string) string {
+	mac := hmac.New(sha256.New, []byte(clientSecret))
+	mac.Write([]byte(username + clientId))
+
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
 func main() {
+	cognitoClientID := os.Getenv("COGNITO_CLIENT_ID")
+
+	sessionCookie := Cookie{Name: "cognito-session"}
+	tokenCookie := Cookie{Name: "cognito-token"}
+
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+	cognito := cognitoidentityprovider.New(sess)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.LoadHTMLFiles("home.html")
+
+	r.POST("/login", func(c *gin.Context) {
+		loginReqBody := UserCredentials{}
+		err := c.BindJSON(&loginReqBody)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		flow := aws.String(flowUsernamePassword)
+		params := map[string]*string{
+			"USERNAME": aws.String(loginReqBody.Username),
+			"PASSWORD": aws.String(loginReqBody.Password),
+		}
+
+		res, err := cognito.InitiateAuth(&cognitoidentityprovider.InitiateAuthInput{
+			AuthFlow:       flow,
+			AuthParameters: params,
+			ClientId:       aws.String(cognitoClientID),
+		})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		if res.Session != nil {
+			sessionCookie.Set(c, *res.Session)
+		}
+
+		if res.AuthenticationResult == nil {
+			c.JSON(http.StatusForbidden, gin.H{
+				"challenge": res.ChallengeName,
+				"params":    res.ChallengeParameters,
+			})
+			return
+		}
+
+		tokenCookie.Set(c, *res.AuthenticationResult.IdToken)
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"msg": "ok",
+		})
+		// TODO: redirect to home
+	})
+
+	r.POST("/changepassword", func(c *gin.Context) {
+		reqBody := UserCredentials{}
+		err := c.BindJSON(&reqBody)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		session, err := sessionCookie.Get(c)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		res, err := cognito.RespondToAuthChallenge(&cognitoidentityprovider.RespondToAuthChallengeInput{
+			ChallengeName: aws.String("NEW_PASSWORD_REQUIRED"),
+			ClientId:      aws.String(cognitoClientID),
+			ChallengeResponses: map[string]*string{
+				"NEW_PASSWORD": aws.String(reqBody.Password),
+				"USERNAME":     aws.String(reqBody.Username),
+			},
+			Session: aws.String(session),
+		})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		tokenCookie.Set(c, *res.AuthenticationResult.IdToken)
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "ok",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	r.GET("/home", func(c *gin.Context) {
+		_, err := tokenCookie.Get(c)
+		if err != nil {
+			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			return
+		}
+
+		// validate token
+
+		// get the username from token
+
+		c.HTML(http.StatusOK, "home.html", gin.H{
+			"username": "John Doe", // Pass any required data to the template
+		})
+	})
+
+	r.Run()
 }

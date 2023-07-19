e81b8ef5a51fd997b272e1f2d0eb79b79d4ababa
anavollu <anavollu@id.uff.br>
2023-07-19 10:41:53 -0300

    separate cookie

--- main.go
+++ main.go
@@ -13,44 +13,6 @@ import (
 	"github.com/hiepd/cognito-go"
 )
 
-// TODO: Cookie on different file
-type Cookie struct {
-	Name string
-}
-
-func (ck Cookie) Set(c *gin.Context, token string) {
-	cookie := http.Cookie{
-		Name:     ck.Name,
-		Value:    token,
-		HttpOnly: true,
-		SameSite: http.SameSiteStrictMode,
-		Secure:   true,
-		Path:     "/",
-	}
-	http.SetCookie(c.Writer, &cookie)
-}
-
-func (ck Cookie) Get(c *gin.Context) (string, error) {
-	cookie, err := c.Request.Cookie(ck.Name)
-	if err != nil {
-		return "", err
-	}
-	return cookie.Value, nil
-}
-
-func (ck Cookie) Clear(c *gin.Context) {
-	cookie := http.Cookie{
-		Name:     ck.Name,
-		Value:    "",
-		HttpOnly: true,
-		SameSite: http.SameSiteStrictMode,
-		Secure:   true,
-		Path:     "/",
-		MaxAge:   -1,
-	}
-	http.SetCookie(c.Writer, &cookie)
-}
-
 const flowUsernamePassword = "USER_PASSWORD_AUTH"
 const flowRefreshToken = "REFRESH_TOKEN_AUTH"
 
